c5c578056986bae7bd772ef03ec888f9278471a8
Sanny Do <sanny.do94@gmail.com>
2020-08-25 02:18:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
- name_hash.sort_by { |key, value| key| do
+ name_hash.sort_by { |key, value| key } do
   if name_hash == smallest
     puts "Smallest number is  #{key}"
   end
