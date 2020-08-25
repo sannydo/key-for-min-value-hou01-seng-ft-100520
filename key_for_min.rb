7814854e4b7abf0b6af1b15c76a7216fca689473
Sanny Do <sanny.do94@gmail.com>
2020-08-25 01:37:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   count = 0
  name_hash.count do
   if name_hash == smallest
-    puts "Smallest number is  #{key}"
+    puts "Smallest number is  #{key} and #{value}"
   end
  end
 end
